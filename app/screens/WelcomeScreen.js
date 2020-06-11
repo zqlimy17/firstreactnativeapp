2b801d64c5eec0a6be679a9602022b0e1650e36c
zqlimy17 <zqlimy@gmail.com>
2020-06-12 02:00:39 +0800

    created image screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import colors from "../config/colors";
 
-function WelcomeScreen() {
+const WelcomeScreen = () => {
     return (
         <ImageBackground
             style={styles.background}
@@ -18,7 +19,7 @@ function WelcomeScreen() {
             <View style={styles.registerButton}></View>
         </ImageBackground>
     );
-}
+};
 
 const styles = StyleSheet.create({
     background: {
@@ -29,19 +30,17 @@ const styles = StyleSheet.create({
     logo: {
         width: 100,
         height: 100,
-        // position: "absolute",
-        // top: 70,
     },
     logoContainer: { position: "absolute", top: 70, alignItems: "center" },
     loginButton: {
         width: "100%",
         height: 70,
-        backgroundColor: "#fc5c65",
+        backgroundColor: colors.primary,
     },
     registerButton: {
         width: "100%",
         height: 70,
-        backgroundColor: "#4ecdc4",
+        backgroundColor: colors.secondary,
     },
 });
 
