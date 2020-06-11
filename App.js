839c604ef78d6e8bb177e6aba445387a68cff87f
zqlimy17 <zqlimy@gmail.com>
2020-06-12 01:11:52 +0800

    flex

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function App() {
-    let x = 1;
     return (
-        <View style={styles.container}>
-            <Text>Hello React Native</Text>
+        <View
+            style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center", // Primary Axis (x-axis)
+                alignItems: "center", // Cross Axis (y-axis)
+            }}
+        >
+            <View
+                style={{
+                    backgroundColor: "dodgerblue",
+                    width: 100,
+                    height: 300,
+                }}
+            />
+            <View
+                style={{ backgroundColor: "gold", width: 100, height: 200 }}
+            />
+            <View
+                style={{ backgroundColor: "tomato", width: 100, height: 100 }}
+            />
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
